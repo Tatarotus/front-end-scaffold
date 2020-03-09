c4aab55e26d318ac85aed164a91f45ec32842a7c
Samuel Dantas <dev@developer.localdomain>
2020-03-09 18:57:30 -0300

    yarn watch also serve

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -22,4 +23,13 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new BrowserSyncPlugin({
+      // browse to http://localhost:3000/ during development,
+      // ./public directory is being served
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['dist'] }
+    })
+  ]
 };
